8a52da02df38dd0940cf5caa516f71d85b267430
StefRCH <95929851+StefRCH@users.noreply.github.com>
2023-12-20 17:52:05 +0100

    Add files via upload

---
+++ src/main/java/fr/insa/soa/RestProject/ClientRest.java
@@ -0,0 +1,17 @@
+package fr.insa.soa.RestProject;
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.Response;
+
+public class ClientRest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Client client = ClientBuilder.newClient();
+		Response response = client.target("http://localhost:8080/RestProject/webapi/comparator/longueur/Toulouse").request().get();
+		System.out.println(response.readEntity(String.class));
+
+	}
+
+}
